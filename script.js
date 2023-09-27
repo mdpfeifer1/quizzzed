d28294e270b539d989612358b40bcd78a37d0e38
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2023-09-26 19:42:00 -0500

    modified js and html

--- script.js
+++ script.js
@@ -34,23 +34,85 @@ var score = 0;
 var timeEl = document.querySelector(".time");
 // Grabbing a section of main declared in the HTML Header 
 var mainEl = document.getElementById("main");
+// Grabbing the display for questions
 var displayEl = document.getElementById("display");
-var questionsInd = 1
+// Index of Questions
+var questionsInd = 0;
+// Grabbing list element
 var listEl = document.getElementById("list");
+// Timer 
+var secondsLeft = 120; 
+// Quiz scores
+var quizScores = document.getElementById("quiz-score"); 
+
+document.getElementById("final-section").setAttribute("style", "display:none");
+
+function checkAnswer(e)
+{
+    e.preventDefault();
+    //alert("inside test");
+    var userChoice = this.getAttribute("data-answer");
+    console.log(userChoice);
+
+    if(userChoice === questions[questionsInd].answer)
+    {
+        score = score + 10;
+        questionsInd++;
+          if(questionsInd === questions.length){
+            return;
+        } else {
+            questionsLeft();
+        }
+    }
+    else{
+        secondsLeft = secondsLeft - 10;
+        if(secondsLeft <= 0)
+        {
+            return;
+        }
+
+        questionsInd++;
+          if(questionsInd === questions.length){
+            return;
+        } else {
+            questionsLeft();
+        }
+    }
+}
+
+function stopQuestions(){
+   //hide questions coccc
+   //show final-section  with textbox and button to enter initials
+   //once user enters initials and click on submit button you will be storing initials and score into localstorage
+   //get all the initials and score saved into localstorage and display it on the screen
+}
 
 // When I press the start button 
 function questionsLeft() {
+    listEl.textContent = "";
     document.getElementById("display-question").textContent=questions[questionsInd].question
     for (var i=0; i < questions[questionsInd].choices.length; i++){
         var li = document.createElement("li")
-        li.textContent = questions[questionsInd].choices[i]
-        listEl.append(li)
-        console.log(questions[questionsInd].choices[i]);
-    }
-    }
- 
+        li.textContent = questions[questionsInd].choices[i];
+        var btn = document.createElement("button");
+        btn.setAttribute("data-answer", questions[questionsInd].choices[i]);
+        btn.onclick = checkAnswer;
+        btn.append(li);
+        listEl.append(btn)};
+        /*listEl.addEventListener("click", function(){
+            if (questions.choices===questions.answers){ 
+            score ++;
+            } else {
+            secondsLeft-=10;
+          }
+          )} */
+          
+        }
+        
+    
+
 // countdown 
-var secondsLeft = 120; 
+ //TODO make an if statement that says if choice === !answer deduct 10 from time-left until time =0
 
 function setTime(event) {
     var timerInterval = setInterval(function (){
@@ -64,7 +126,7 @@ function setTime(event) {
     }, 1000);
 }
 function sendMessage()  {
-    timeEl.textContent = "";
+    timeEl.textContent = "Out of time";
     
 }
 
@@ -75,7 +137,25 @@ function start() {
     displayEl.style.display="block"
 }
  startButton.addEventListener("click", start)
+ 
 
+
+//TODO make a conditional that says once time-left >= 0 alert you lose
+
+
+//TODO make a if statement that says once last question is answered take the time left as a score and put it in local storage
+
+
+//TODO once they get their score let them add 3 letters for initials and add that as a key and use the score as the value
+
+
+//TODO make a high-score list on page using a getItem from both the key and value and display that.
+
+
+//TODO create a function that will display the next question and choices once they answer the following question
+
+
+//TODO create a start button that wont display the questions until pressed
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 
